dc49e7d046648fbad26bb7438145d781525da98f
harsha <104590330+harsharaju92@users.noreply.github.com>
2022-12-23 12:00:38 +0530

    Update hello-openshift-for-developers.go

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift for Madhuri!"
+		response = "Hello OpenShift for Madhuri lovers!"
 	}
 
 	fmt.Fprintln(w, response)
